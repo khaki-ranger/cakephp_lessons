71c83ebbdb77cd96e1cb4ef0753e905fa8c40b63
khaki-ranger <yamanogomora@gmail.com>
2019-01-05 01:13:35 +0900

    #11 レイアウトを変更してみよう

--- myblog/src/Controller/PostsController.php
+++ myblog/src/Controller/PostsController.php
@@ -6,12 +6,8 @@ class PostsController extends AppController
 {
   public function index()
   {
-    // $posts = $this->Posts->find('all');
-    $posts = $this->Posts->find('all')
-          ->order(['title' => 'DESC'])
-          ->limit(2)
-          ->where(['title like' => '%3']);
-    // $this->set('posts', $posts);
+    $this->viewBuilder()->layout('my_layout');
+    $posts = $this->Posts->find('all');
     $this->set(compact('posts'));
   }
 }
