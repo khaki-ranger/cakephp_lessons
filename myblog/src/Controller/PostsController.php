b833b97312aa1bd52bcc4f1338e318c9b0381531
khaki-ranger <yamanogomora@gmail.com>
2019-01-05 02:08:00 +0900

    #15 view.ctpを作っていこう

--- myblog/src/Controller/PostsController.php
+++ myblog/src/Controller/PostsController.php
@@ -9,4 +9,10 @@ class PostsController extends AppController
     $posts = $this->Posts->find('all');
     $this->set(compact('posts'));
   }
+
+  public function view($id = null)
+  {
+    $post = $this->Posts->get($id);
+    $this->set(compact('post'));
+  }
 }
