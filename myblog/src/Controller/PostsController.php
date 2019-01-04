ac1ae6d232b3266ea5607b00bf21368650ea0111
khaki-ranger <yamanogomora@gmail.com>
2019-01-05 01:04:30 +0900

    #10 ルーティングの設定をしていこう

--- myblog/src/Controller/PostsController.php
+++ myblog/src/Controller/PostsController.php
@@ -1,14 +1,16 @@
 <?php
 
-// /(controller)/(action)/(options)
-
 namespace App\Controller;
 
 class PostsController extends AppController
 {
   public function index()
   {
-    $posts = $this->Posts->find('all');
+    // $posts = $this->Posts->find('all');
+    $posts = $this->Posts->find('all')
+          ->order(['title' => 'DESC'])
+          ->limit(2)
+          ->where(['title like' => '%3']);
     // $this->set('posts', $posts);
     $this->set(compact('posts'));
   }
