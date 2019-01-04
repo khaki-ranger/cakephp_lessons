c96478eb475cdb8bfad3acccb649897d09b5c70b
khaki-ranger <yamanogomora@gmail.com>
2019-01-05 01:30:16 +0900

    #13 Elementsを使ってみよう

--- myblog/src/Controller/PostsController.php
+++ myblog/src/Controller/PostsController.php
@@ -6,7 +6,6 @@ class PostsController extends AppController
 {
   public function index()
   {
-    $this->viewBuilder()->layout('my_layout');
     $posts = $this->Posts->find('all');
     $this->set(compact('posts'));
   }
