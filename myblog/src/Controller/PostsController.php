759dc728d786906cac046469c94c6c25623a87a6
khaki-ranger <yamanogomora@gmail.com>
2019-01-05 02:52:51 +0900

    #18 Validationを設定してみよう

--- myblog/src/Controller/PostsController.php
+++ myblog/src/Controller/PostsController.php
@@ -21,8 +21,11 @@ class PostsController extends AppController
     $post = $this->Posts->newEntity();
     if ($this->request->is('post')) {
       $post = $this->Posts->patchEntity($post, $this->request->data);
-      $this->Posts->save($post);
-      return $this->redirect(['action'=>'index']);
+      if ($this->Posts->save($post)) {
+        return $this->redirect(['action'=>'index']);
+      } else {
+        // error
+      }
     }
     $this->set(compact('post'));
   }
